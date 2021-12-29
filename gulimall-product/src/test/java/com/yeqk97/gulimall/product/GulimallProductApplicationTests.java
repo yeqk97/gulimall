628c0f01ef19153e70f666edadac3b5f82ed6589
qkye <yeqk97@gmail.com>
2021-12-29 16:33:20 +0100

    Initial project structure

---
+++ gulimall-product/src/test/java/com/yeqk97/gulimall/product/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeqk97.gulimall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
